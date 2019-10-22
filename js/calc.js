3cc8de4970b4a537d073960087290277b3c7f49b
[InnCreator] <[engbethany4@gmail.com]>
2019-10-22 19:41:50 +0300

    start of something new

---
+++ js/calc.js
@@ -0,0 +1,12 @@
+function Sum(num1, num2) {
+    return num1 + num2;
+}
+function Sub(num1, num2) {
+    return num1 - num2;
+}
+function Multiplication(num1, num2) {
+    return num1 * num2;
+}
+function Division(num1, num2) {
+    return num1 / num2;
+}
