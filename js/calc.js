c4ea9c9daefb260e3a9181cae1c1cdffe664c9a9
[InnCreator] <[engbethany4@gmail.com]>
2019-10-23 12:14:32 +0300

    calc.js

--- js/calc.js
+++ js/calc.js
@@ -12,25 +12,29 @@
 // }
 
 // let num1 = ('1', '2', '3', '4', '5', '6', '7', '8', '9')
-
+// if (!action) {
+//          console.log('number key!')
+//      }
+//      if (
+//          action === 'add' ||
+//          action === 'subtract' ||
+//          action === 'multiply' ||
+//          action === 'divide'
+//      ) {
+//          console.log('operator key!')
+//      }
+//      if (action === 'clear') {
+//          console.log('clear key!')
+//      }
+//      if (action === 'calculate') {
+//          console.log('equal key!')
+//      }
 
 const calculator = document.querySelector('.screen')
-const keys = calculator.querySelector('.button')
+const keys = document.querySelector('.item')
 
 keys.addEventListener('click', e => {
- if (e.target.matches('button')) {
-     const key = e.target
-     const action = key.dataset.action
-     if (!action) {
-         console.log('number key!')
-     }
-     if (
-  action === 'add' ||
-  action === 'subtract' ||
-  action === 'multiply' ||
-  action === 'divide'
-) {
-  console.log('operator key!')
-}
- }
+    if (e.target.matches('.item5')) {
+     console.log('reached a number')
+    }
 })
