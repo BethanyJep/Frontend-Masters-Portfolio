9e89e84bff677507761bd27f565a68e54e21a600
[InnCreator] <[engbethany4@gmail.com]>
2019-10-23 09:30:43 +0300

    calc

--- js/calc.js
+++ js/calc.js
@@ -1,12 +1,36 @@
-function Sum(num1, num2) {
-    return num1 + num2;
+// function Sum(num1, num2) {
+//     return num1 + num2;
+// }
+// function Sub(num1, num2) {
+//     return num1 - num2;
+// }
+// function Multiplication(num1, num2) {
+//     return num1 * num2;
+// }
+// function Division(num1, num2) {
+//     return num1 / num2;
+// }
+
+// let num1 = ('1', '2', '3', '4', '5', '6', '7', '8', '9')
+
+
+const calculator = document.querySelector('.screen')
+const keys = calculator.querySelector('.button')
+
+keys.addEventListener('click', e => {
+ if (e.target.matches('button')) {
+     const key = e.target
+     const action = key.dataset.action
+     if (!action) {
+         console.log('number key!')
+     }
+     if (
+  action === 'add' ||
+  action === 'subtract' ||
+  action === 'multiply' ||
+  action === 'divide'
+) {
+  console.log('operator key!')
 }
-function Sub(num1, num2) {
-    return num1 - num2;
-}
-function Multiplication(num1, num2) {
-    return num1 * num2;
-}
-function Division(num1, num2) {
-    return num1 / num2;
-}
+ }
+})
