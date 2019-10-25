d62d7a7d093ef5c5990620cec293e3ddf3d9187f
[InnCreator] <[engbethany4@gmail.com]>
2019-10-25 12:53:17 +0300

    updates

--- js/calc.js
+++ js/calc.js
@@ -1,24 +1,3 @@
-// function Sum(num1, num2) {
-//     return num1 + num2;
-// }
-// function Sub(num1, num2) {
-//     return num1 - num2;
-// }
-// function Multiplication(num1, num2) {
-//     return num1 * num2;
-// }
-// function Division(num1, num2) {
-//     return num1 / num2;
-// }
+let keys = document.querySelectorAll('button')
+keys = Array.from(keys).forEach(key => key.addEventListener('click', () => console.log('hell', key)));
 
-// let num1 = 
-function numbers() {
-    const num1 = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
-    parseInt(num1)
-}
-const calculator = document.querySelector('.screen');
-
-const keys = document.getElementsByClassName("square");
-keys.addEventListener("click", function () {
-    alert("Hello World!");
-});
