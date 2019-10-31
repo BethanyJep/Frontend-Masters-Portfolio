4fd92045fdfb2b7335ac178637cb025d1bb9feb5
[InnCreator] <[engbethany4@gmail.com]>
2019-10-31 10:15:52 +0300

    dogs, godamn, freaking dogs

--- js/doggo.js
+++ js/doggo.js
@@ -1,6 +1,7 @@
 const breedsUrl = "https://dog.ceo/api/breeds/image/random";
-
 const dog = document.querySelector(".dog")
+const breedList = "https://dog.ceo/api/breeds/list/all"
+const searchDogs = getElementById('search')
 
 function addNewDog() {
     const promise = fetch(breedsUrl);
@@ -13,18 +14,36 @@ function addNewDog() {
             const img = document.createElement("img");
             img.src = processedResponse.message;
             img.alt = "I hate dogs, please take them all";
+            img.style.width = 'auto';
+            img.style.height = '200px';
+            img.style.margin = '10px';
+            img.style.borderRadius = '10px';
             dog.appendChild(img);
         })
 }
 let buttonOn = document.querySelector(".add-dog")
-
 buttonOn.addEventListener('click', addNewDog);
 
-// buttonOn.addEventListener('load', function () {
-//     const load = document.createElement("img")
-//     load.src = "images/load.gif";
-//     load.alt = "infity loading.io";
-//     return load;
-// })
+function searchBreed() {
+    const promise = fetch(breedList);
+    promise
+        .then(function (respond) {
+            const processPromise = respond.json()
+            return processPromise;
+        })
+        .then(function (processedResponse) {
+            const searchDogs = getElementById('search')
+            searchDogs.value = processedResponse.message;
+            
+        })
+        .then(function () {
+            
+        })
+}
 
-addNewDog.onloadstart = function () {}
+function loading() {
+    const load = document.createElement("img")
+    load.src = "images/load.gif";
+    load.alt = "infity loading.io";
+    return load;
+}
